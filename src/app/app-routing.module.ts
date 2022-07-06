dc60705886fed461d3046096cb57045883cd0976
Luis Miguel Orozco Camacho <oclm.1999.cuentas@gmail.com>
2022-07-06 18:13:56 -0500

    todo bien hasta el video 53

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+//Modulos
+import { AuthRoutingModule } from './auth/auth.routing';
+import { PagesRoutingModule } from './pages/pages.routing';
+
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+
+const routes: Routes = [
+  //path: '/dashboard' PagesRouting
+  //path: '/auth' AuthRouting
+  //path: '/medicos' MedicosRouting
+  //path: '/compras' ComprasRouting
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '**', component: NopagefoundComponent }
+]
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot( routes ),
+    AuthRoutingModule,
+    PagesRoutingModule
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
